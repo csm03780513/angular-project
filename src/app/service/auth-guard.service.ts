82c283604dc47b19da0d6fcf8ca1ed84ec7d3ae9
rmurray <rmurray@safaricom.co.ke>
2019-06-05 23:20:17 +0300

    login logout components

--- src/app/service/auth-guard.service.ts
+++ src/app/service/auth-guard.service.ts
@@ -12,8 +12,8 @@ export class AuthGuardService implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     if (this.authenticationService.isUserLoggedIn) {
       return true;
-      this.router.navigate(['login']);
     } else {
+      this.router.navigate(['login']);
       return false;
     }
   }
